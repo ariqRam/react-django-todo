98980af3c1282eeee6ce53fee00616e2c3c1cf7f
Louis JZ <louis.ljz08@gmail.com>
2022-06-30 15:50:04 +0700

    Create endpoints and database

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    # username = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
