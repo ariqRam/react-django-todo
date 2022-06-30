cf44478cee9a1fc63a9f672ceea6a55574eec687
Louis JZ <louis.ljz08@gmail.com>
2022-06-30 14:39:24 +0700

    Louis'es Starting Files

--- backend/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import my_api_view
-
-urlpatterns = [
-    path('', my_api_view)
-]
--- backend/todo/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def my_api_view(request):
-    return HttpResponse('Hello client!')
