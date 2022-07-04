80bffed58cc3afea2d9ec6a02ca07298fb85ad97
Ariq Ramdhany <ariqathallah38@gmail.com>
2022-07-04 13:49:34 +0700

    working frontend + backend

--- frontend/src/components/TodoInput.jsx
+++ frontend/src/components/TodoInput.jsx
@@ -1,56 +1,65 @@
 import { useState } from "react";
-import Axios from 'axios';
-import { useEffect } from "react";
+import Axios from "axios";
 
-import '../static/todoInput.css';
-import mockupDb from '../static/mockupDb.json';
+import "../static/todoInput.css";
+import mockupDb from "../static/mockupDb.json";
 
 const TodoInput = (props) => {
-    const [newTodo, setNewTodo] = useState({});
-    const [newTitle, setNewTitle] = useState('');
-    const [newDesc, setNewDesc] = useState('');
+  const [newTitle, setNewTitle] = useState("");
+  const [newDesc, setNewDesc] = useState("");
 
-    const handleChange = (event) => {
-        // handle the change in todo title
-        setNewTitle(event.target.value);
-        // const lastTodo = mockupDb[mockupDb.length - 1];
-        // setNewTodo({ id: mockupDb ? lastTodo.id + 1 : 1, title: event.target.value, description: 'Please do it', completed: false });
-    }
+  const handleChange = (event) => {
+    // handle the change in todo title
+    setNewTitle(event.target.value);
+    // const lastTodo = mockupDb[mockupDb.length - 1];
+    // setNewTodo({ id: mockupDb ? lastTodo.id + 1 : 1, title: event.target.value, description: 'Please do it', completed: false });
+  };
 
-    const handleDescChange = (event) => {
-        setNewDesc(event.target.value);
-    }
+  const handleDescChange = (event) => {
+    setNewDesc(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        const lastTodo = mockupDb[mockupDb.length - 1];
-        const newTodo = { id: mockupDb ? lastTodo.id + 1 : 1, title: newTitle, description: newDesc, completed: false }
-        event.preventDefault();
-        const newMockupDb = [...mockupDb, newTodo];
-        console.log('submit!!');
-        Axios.post('http://localhost:8000/mockup/', newMockupDb)
-            .then(res => {
-                console.log(res);
-            });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const lastTodo = mockupDb[mockupDb.length - 1];
+    const newTodo = {
+      id: mockupDb[0] ? lastTodo.id + 1 : 1,
+      title: newTitle,
+      description: newDesc,
+      completed: false,
+    };
+    console.log(newTodo);
+    const newMockupDb = [...mockupDb, newTodo];
+    console.log("submit!!");
+    Axios.post("http://localhost:8000/mockup/", newMockupDb).then((res) => {
+      console.log(res);
+    });
 
-        setNewTitle('');
-        setNewDesc('');
-        props.initializeTodos();
-    }
+    setNewTitle("");
+    setNewDesc("");
+    props.initializeTodos();
+  };
 
-    useEffect(() => {
-
-    }, [newTodo]);
-
-    return (
-        <>
-            <form className="inputGroup">
-                <h1>Add a Todo</h1>
-                <input type="text" placeholder="Enter a new todo title . . ." onChange={handleChange} value={newTitle} />
-                <input type="text" placeholder="Enter a description . . ." onChange={handleDescChange} value={newDesc} />
-                <input type="submit" onClick={handleSubmit} />
-            </form>
-        </>
-    );
-}
+  return (
+    <>
+      <form className="inputGroup">
+        <h1>Add a Todo</h1>
+        <input
+          type="text"
+          placeholder="Enter a new todo title . . ."
+          onChange={handleChange}
+          value={newTitle}
+        />
+        <input
+          type="text"
+          placeholder="Enter a description . . ."
+          onChange={handleDescChange}
+          value={newDesc}
+        />
+        <input type="submit" onClick={handleSubmit} />
+      </form>
+    </>
+  );
+};
 
 export default TodoInput;
